d6e9472b5c21837ee04e3280d7b56b605f5a49aa
Omokiti <emamuzoomokiti@gmail.com>
2023-06-06 17:31:11 +0100

    feat:Added API integration

--- src/app/components/search.js
+++ src/app/components/search.js
@@ -6,21 +6,24 @@ import { useState } from 'react';
 
 export default function Search(){
     // const[authors,setAuthors] = useState({results:[]});
-    const[authors,setAuthors] = useState({products:[] });
+    const[authors,setAuthors] = useState({results:[]});
     const [searchInput,setSearchInput] = useState('');
     const [modal,setModal] = useState(false);
     const searchItems = (searchValue) => {
         setSearchInput(searchValue)
+        
       }
     //   https://api.openalex.org/authors?search=${searchInput}
     //   https://dummyjson.com/products/search?q=${searchInput}
 const findAuthor=()=>{
-    const url=`https://dummyjson.com/products/search?q=${searchInput}`
+   
+    const url=`https://api.openalex.org/authors?search=${searchInput}`
     fetch(url)
     .then( (response)=> response.json())
     .then((data) => {
         console.log(data)
         setAuthors(data)
+        
      })
      .catch((err=>{
         console.log(err)
@@ -34,9 +37,9 @@ const findAuthor=()=>{
             <button className='searchbtn' onClick={findAuthor}>Search</button>
             </div>
             <div className='max-w-screen-md grid gap-4 grid-cols-1 m-auto md:grid-cols-2 searchdetails'>
-                { authors.products.map((product)=>
-                <div key={product.id}>
-                    <p>{product.title}</p>
+                { authors.results.map((result)=>
+                <div className="bg-zinc-100 dark:bg-darkOffset dark:text-gray-100 rounded-md p-3 hover:bg-gray-100 transition cursor-copy border-zinc-200 border dark:border-zinc-800"key={result.id}>
+                    <p>{result.display_name}</p>
                 </div>
                 )}
                
@@ -44,3 +47,4 @@ const findAuthor=()=>{
             </>
     )
 }
+
